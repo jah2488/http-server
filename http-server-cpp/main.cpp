4be9d3a859d5d6d1ca08f5feff337ddee9cc65c7
Justin Herrick <justin@justinherrick.com>
2012-06-08 12:24:56 -0400

    Initial Commit

---
+++ http-server-cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  http-server-cpp
+//
+//  Created by justin Herrick on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
